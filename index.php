d6c10cb55ebf92debec97c6e5970cfc210809d6d
TmsRvl <tommaso.rovoletto@itiszuccante.edu.it>
2023-02-01 18:50:55 +0100

    fixed some bugs

--- index.php
+++ index.php
@@ -14,9 +14,6 @@ $nodes = $xpath->query("//td//a//text()");
 //ritorna altre info
 $info = $xpath->query("//td[@class='small']//text()");
 
-
-
-
 $output;
 $squads = getSquadInfo();
 
@@ -73,11 +70,10 @@ function getSquadRoster($url){
     $titles = $xpath->query("//div[@id='team']//th//text()");
     //ritorna i dettagli dei giocatori
     $link = $xpath->query("//div[@id='team']//td//text()");
-    header("Content-type: text/plain");
 
     $keys;
     $values;
-    $out; 
+    $out = []; 
 
     foreach ($titles as $i => $node) {
         if(!removeWhiteSpace($node->nodeValue) == '')
@@ -85,29 +81,43 @@ function getSquadRoster($url){
     }
 
     $j = 0;
+    $n = 0;
     foreach ($link as $i => $node) {
         if($j < 2){
             if(is_numeric(removeWhiteSpace($node->nodeValue)) || !removeWhiteSpace($node->nodeValue) == ''){
-                $out[$keys[0]] .= removeWhiteSpace($node->nodeValue) . " ";
+                $out[$keys[0]] = (!isset($out[$keys[0]])) ? removeWhiteSpace($node->nodeValue) : $out[$keys[0]].' '. removeWhiteSpace($node->nodeValue);
                 $j++;
             }
         }else{
-            if(is_numeric(removeWhiteSpace($node->nodeValue)) || !removeWhiteSpace($node->nodeValue) == '')
-                $values[] = removeWhiteSpace($node->nodeValue);
+            if(is_numeric(removeWhiteSpace($node->nodeValue)) || !removeWhiteSpace($node->nodeValue) == ''){
+                if($n%8 == 7 && is_numeric($node->nodeValue)){
+                    $values[] = '-';
+                    $values[] = removeWhiteSpace($node->nodeValue); 
+                    $n+=2;
+                }else{
+                    $values[] = removeWhiteSpace($node->nodeValue);
+                    $n++;
+                }
+            }
         }
     }
 
+    $missing = 0;
     for($i=0; $i<(sizeof($values)/8); $i++){
-        $i = (is_numeric($values[($i*8)])) ? $i : $i-1;
-        $out[$keys[1]][$i] = array(
-            $keys[2] => (is_numeric($values[($i*8)])) ? $values[($i*8)] : '-',
-            $keys[3] => $values[($i*8)+1] . " " . $values[($i*8)+2],
-            $keys[4] => $values[($i*8)+3],
-            $keys[5] => $values[($i*8)+4],
-            $keys[6] => $values[($i*8)+5],
-            $keys[7] => $values[($i*8)+6],
-            $keys[8] => $values[($i*8)+7]
-        );
+       $index = $i*8;
+       if(!is_numeric($values[$index])) {
+           $index-=$missing+1;
+           $missing++;
+       }
+       $out[$keys[1]][$i] = array(
+           $keys[2] => (!is_numeric($values[($i*8)]))? '-' : $values[($i*8)],
+           $keys[3] => $values[$index+1] . " " . $values[$index+2],
+           $keys[4] => $values[$index+3],
+           $keys[5] => $values[$index+4],
+          $keys[6] => $values[$index+5],
+          $keys[7] => $values[$index+6],
+          $keys[8] => $values[$index+7]
+      );
     }
 
     return $out;
--- prova.php
+++ prova.php
@@ -1,6 +1,6 @@
 <?php
 libxml_use_internal_errors(true);
-$url = "https://www.legabasket.it/lba/squadre/2022/1538/banco-di-sardegna-sassari";
+$url = "https://www.legabasket.it/lba/squadre/2022/1533/ea7-emporio-armani-milano";
 $dom = new DomDocument;
 $dom->loadHTMLFile($url);
 
@@ -14,7 +14,7 @@ header("Content-type: text/plain");
 
 $keys;
 $values;
-$out; 
+$out = []; 
 
 foreach ($titles as $i => $node) {
     if(!removeWhiteSpace($node->nodeValue) == '')
@@ -22,30 +22,48 @@ foreach ($titles as $i => $node) {
 }
 
 $j = 0;
+$n = 0;
 foreach ($link as $i => $node) {
     if($j < 2){
         if(is_numeric(removeWhiteSpace($node->nodeValue)) || !removeWhiteSpace($node->nodeValue) == ''){
-            $out[$keys[0]] .= removeWhiteSpace($node->nodeValue) . " ";
+            $out[$keys[0]] = (!isset($out[$keys[0]])) ? removeWhiteSpace($node->nodeValue) : $out[$keys[0]].' '. removeWhiteSpace($node->nodeValue);
             $j++;
         }
     }else{
-        if(is_numeric(removeWhiteSpace($node->nodeValue)) || !removeWhiteSpace($node->nodeValue) == '')
-            $values[] = removeWhiteSpace($node->nodeValue);
+        if(is_numeric(removeWhiteSpace($node->nodeValue)) || !removeWhiteSpace($node->nodeValue) == ''){
+            if($n%8 == 7 && is_numeric($node->nodeValue)){
+                $values[] = '-';
+                $values[] = removeWhiteSpace($node->nodeValue); 
+                $n+=2;
+            }else{
+                $values[] = removeWhiteSpace($node->nodeValue);
+                $n++;
+            }
+        }
     }
 }
 
-for($i=0; $i<(sizeof($values)/8); $i++){
+$missing = 0;
+for($i=0; $i<(sizeof($values)/8); $i++){    
+    $index = $i*8;
+    echo $values[$index] . " ";
+    if(!is_numeric($values[$index])) {
+        $index -= $missing+1;
+        $missing++;
+    }
+    echo $values[$index] . "|";
     $out[$keys[1]][$i] = array(
-        $keys[2] => $values[($i*8)],
-        $keys[3] => $values[($i*8)+1] . " " . $values[($i*8)+2],
-        $keys[4] => $values[($i*8)+3],
-        $keys[5] => $values[($i*8)+4],
-        $keys[6] => $values[($i*8)+5],
-        $keys[7] => $values[($i*8)+6],
-        $keys[8] => $values[($i*8)+7]
+        $keys[2] => (!is_numeric($values[$index]))? '-' : $values[$index],
+        $keys[3] => $values[$index+1] . " " . $values[$index+2],
+        $keys[4] => $values[$index+3],
+        $keys[5] => $values[$index+4],
+        $keys[6] => $values[$index+5],
+        $keys[7] => $values[$index+6],
+        $keys[8] => $values[$index+7]
     );
 }
 
+print_r($out);
 
 function removeWhiteSpace($text){
     $text = preg_replace('/\s+/', ' ', $text);
