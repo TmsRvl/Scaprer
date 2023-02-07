a29660d7cbf78d891fcd9243a3fd431011417618
TmsRvl <tommaso.rovoletto@itiszuccante.edu.it>
2023-02-07 18:59:34 +0100

    updated

--- lba/page.js
+++ lba/page.js
@@ -55,9 +55,121 @@ function loadTable(data){
 
 function loadMatchDetail(squads) {
     document.querySelector(".detail").innerHTML = "";
+    var tbl = document.createElement('table');
+    var tbdy = document.createElement('tbody');
+    var tr = document.createElement('tr');
+    var td1 = document.createElement('td');
     fetch("http://localhost/esercizi/scraper/detail.php?d="+squads['Squadra di casa'])
         .then((response) => response.json())
         .then((data) => {
-            
+            td1.appendChild(buildSquadTable(data)); 
         });   
+    tr.appendChild(td1);
+    var td2 = document.createElement('td');
+    fetch("http://localhost/esercizi/scraper/detail.php?d="+squads['Squadra ospite'])
+        .then((response) => response.json())
+        .then((data) => {
+            td2.appendChild(buildSquadTable(data)); 
+        });   
+    tr.appendChild(td2);
+    tbdy.appendChild(tr);
+    tbl.appendChild(tbdy);
+    document.querySelector(".detail").appendChild(tbl);
+}
+
+function buildSquadTable(data) {
+    console.log(data);
+    var tbl = document.createElement('table');
+    var tbdy = document.createElement('tbody');
+
+    //logo
+    var logo = document.createElement('tr');
+    var a = document.createElement('th');
+    a.colSpan = 8;
+    var img = document.createElement('img');
+    img.src = data['Logo'];
+    img.id = 'logo';
+    a.appendChild(img);
+    logo.appendChild(a);
+    tbdy.appendChild(logo);
+
+    //nome squadra
+    var squad = document.createElement('tr');
+    var b = document.createElement('th');
+    b.colSpan = 8;
+    b.innerHTML = data['Name'];
+    squad.appendChild(b);
+    tbdy.appendChild(squad);
+
+    //allenatore
+    var allenatore = document.createElement('tr');
+    var c = document.createElement('th');
+    var d = document.createElement('th');
+    c.colSpan = 4;
+    d.colSpan = 4;
+    var ico = document.createElement('img');
+    ico.src = data['Allenatore']['Displayed_icon'];
+    d.innerHTML = data['Allenatore']['Name'];
+    c.appendChild(ico);
+    allenatore.appendChild(c);
+    allenatore.appendChild(d);
+    tbdy.appendChild(allenatore);
+
+    for (i = 0; i < data['Roster'].length; i++) {
+        if(i == 0){
+            var tr = document.createElement('tr');
+            for(const [key, value] of Object.entries(data['Roster'][i])){
+                var th = document.createElement('th');
+                if(key == 'Displayed_icon')
+                    th.innerHTML = ' ';
+                else
+                    th.innerHTML = key;
+                tr.appendChild(th);
+            }
+            tbdy.appendChild(tr);
+        }
+        var tr = document.createElement('tr');
+        for(const [key, value] of Object.entries(data['Roster'][i])){
+            var td = document.createElement('td');
+            if(key!= 'Displayed_icon'){
+                td.innerHTML = value;
+            }else{
+                let icon = document.createElement('img');
+                icon.src = value;
+                icon.id = 'player';
+                td.appendChild(icon); 
+            }
+            tr.appendChild(td);
+        }
+        tbdy.appendChild(tr);
+    }
+
+    // for (i = 0; i < data.length; i++) {
+    //     if(i == 0){
+    //         var tr = document.createElement('tr');
+    //         for(const [key, value] of Object.entries(data[i])){
+    //             var th = document.createElement('th');
+    //             th.innerHTML = key;
+    //             tr.appendChild(th);
+    //         }
+    //         tbdy.appendChild(tr);
+    //     }
+    //     var tr = document.createElement('tr');
+    //     for(const [key, value] of Object.entries(data[i])){
+    //         var td = document.createElement('td');
+    //         if(key!= 'info'){
+    //             td.innerHTML = value;
+    //         }else{
+    //             let btn = document.createElement('button');
+    //             btn.onclick = () => {loadMatchDetail(value)};
+    //             btn.innerHTML = '&#8617;';
+    //             td.appendChild(btn); 
+    //         }
+    //         tr.appendChild(td);
+    //     }
+    //     tbdy.appendChild(tr);
+    // }
+    
+    tbl.appendChild(tbdy);
+    return tbl;
 }
